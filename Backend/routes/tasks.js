0762fd428c5fda2a5cfe40c306d8d7b2b024fe12
venkateshprasad598 <venkateshprasad598@gmail.com>
2022-01-20 12:57:32 +0530

    add

--- Backend/routes/tasks.js
+++ Backend/routes/tasks.js
@@ -3,30 +3,53 @@ const express = require("express");
 const router = express.Router();
 const Task = require("../model/schema");
 
+router.get("/static", async (req, res) => {
+  let products = await Task.find({})
+    .sort("name price")
+    .select("name price")
+    .skip(3)
+    .limit(10);
+
+  res.status(200).json({ products, length: products.length });
+});
 router.get("/", async (req, res) => {
-  const products = await Task.find({});
-  console.log(products);
-  res.status(200).json({ products });
-  // throw new Error("ejkfbkjb kjfbjke");
-});
-router.post("/", async (req, res) => {
-  try {
-    const task = await Task.create(req.body);
-    res.send(task);
-  } catch (err) {
-    res.send(err);
+  console.log(req.query);
+  const { featured, company, name, sort, field, price } = req.query;
+  const obj = {};
+  if (featured) {
+    obj.featured = featured;
   }
+  if (company) {
+    obj.company = company;
+  }
+  if (name) {
+    obj.name = { $regex: name };
+  }
+  if (price) {
+    obj.price = { $gt: price };
+  }
+  const result = Task.find(obj);
+
+  //SORTING
+  // WE HAVE REMOVED AWAIT HERE BECAUSE WE HAVE TO ATTACH IT TO THE FIND FUNCTION ITSELF
+  if (sort) {
+    let newSort = sort.split(",").join(" ");
+    result.sort(newSort);
+  }
+
+  if (field) {
+    let newSort = field.split(",").join(" ");
+    result.select(newSort);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result.skip(skip).limit(limit);
+
+  let products = await result;
+  res.status(200).json({ products, length: products.length });
 });
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  res.json(id);
-});
-router.patch("/:id", (req, res) => {
-  const id = req.params.id;
-  res.send({ id });
-});
-router.delete("/:id", (req, res) => {
-  const id = req.params.id;
-  res.send({ id });
-});
+
 module.exports = router;
